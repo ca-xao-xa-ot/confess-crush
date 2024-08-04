f216f33d52f25c4d45e5244ebfd2ec84892e56be
ca-xao-xa-ot <thitvitham1314@gmail.com>
2024-08-04 11:24:24 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'luong linhtretrou',
+    introTitle: 'chi hin dangiu',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
